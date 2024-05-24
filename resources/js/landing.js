2c299898e44c62bdae393e56416adbf5681062b8
mattdig <mattdigiuseppe@gmail.com>
2024-05-24 16:41:22 -0400

    account for Classic, NCE, and NSE environments

--- resources/js/landing.js
+++ resources/js/landing.js
@@ -1,16 +1,16 @@
 let url = window.top.location.href;
-let match_instructor = url.match(/\/lessons\/(\d+)\/topics\/(\d+)/);
-let match_student = url.match(/\/enhancedSequenceViewer\/(\d+)\?url=(\S+)activity%2F(\d+)/)
+let match_standard = url.match(/\/(?:lessons|content)\/(\d+)\/topics\/(\d+)/);
+let match_newstudentexperience = url.match(/\/enhancedSequenceViewer\/(\d+)\?url=(?:\S+)activity%2F(\d+)/)
 
 let ORG_UNIT_ID = 0;
 let TOPIC_ID = 0;
 
-if (match_instructor != null) {
-    ORG_UNIT_ID = match_instructor[1];
-    TOPIC_ID = match_instructor[2];
-} else if (match_student != null) {
-    ORG_UNIT_ID = match_student[1];
-    TOPIC_ID = match_student[3];
+if (match_standard != null) {
+    ORG_UNIT_ID = match_standard[1];
+    TOPIC_ID = match_standard[2];
+} else if (match_newstudentexperience != null) {
+    ORG_UNIT_ID = match_newstudentexperience[1];
+    TOPIC_ID = match_newstudentexperience[2];
 }
 
 const bs = new Brightspace(ORG_UNIT_ID);
